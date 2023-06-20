c794c56b095eded9397326003d5c2ce744b9fbab
Allen Razdow <arazdow@truenum.com>
2023-06-20 19:02:03 -0400

    Combined ingest into Tnum, delete works

--- TNUM.py
+++ TNUM.py
@@ -3,6 +3,7 @@ import pandas as pd
 import inspect
 import json
 import datetime
+import re
 
 # truenumber class
 
@@ -57,7 +58,6 @@ class Tnum(object):
             raw_nspaces = payload["numberspaces"]
             for space in raw_nspaces:
                 Tnum.nspaces.append(space.split('/')[1])
-            print(Tnum.nspaces)
             Tnum.nspace = Tnum.nspaces[0]
 
             print("Available spaces:", Tnum.nspaces)
@@ -65,11 +65,15 @@ class Tnum(object):
 
     @staticmethod
     def createSpace(name):
-        result = requests.post(
+        resultraw = requests.post(
             f"http://{Tnum.endpoint}/v2/numberflow/numberspace",
             json={"numberspace": name}
         )
-        return result.json()
+        result = resultraw.json()
+        if "code" in result: 
+            print("Creating " + name + " " + result["code"])
+        else:
+            print("Created " + name)
 
     @staticmethod
     def setSpace(name="testspace"):
@@ -205,10 +209,10 @@ class Tnum(object):
 
         response = requests.delete(url, params=args, json=body, headers=headers)
         result = response.json()
-        numReturned = len(result["deletedCount"])
+        numReturned = result["deletedCount"]
 
         plural = " match" if numReturned == 1 else " matches"
-        print(f"deleted {numReturned} {plural}")
+        print(f"Deleted {numReturned} {plural}")
 
 
     # Tag tnums specified by a query
@@ -460,3 +464,90 @@ class Tnum(object):
             Nvec = []
 
         return Nvec
+    
+# Ingest group of funcs
+    
+    @staticmethod
+    def tokenize(aString):
+        tok = re.sub(r'\s+', '_', aString.strip())
+        tok = re.sub(r'[^a-zA-Z0-9\'_]', '-', tok)
+        return tok
+
+    @staticmethod
+    def parsePhrase(phr):
+        splt = phr.split()
+        splt.reverse()
+        splt = ':'.join(splt)
+        splt = splt.replace(':of:', '/')
+        return splt
+
+    @staticmethod
+    def ingestDataFrame(df, templates):
+        
+        # inner functions
+        tkn = lambda val: re.sub(r'\s+', '_', val.strip()) if not pd.isnull(val) and isinstance(val, str) else val
+
+        dateTkn = lambda: tkn(str(date()))
+
+        def doTemplates(macros, tmplt, theRow):
+            for macro in macros:
+                fn = re.search(r'\$(.*)\(', macro)
+                if fn is not None:
+                    fn = fn.group(1)
+                else:
+                    fn = ""
+
+                mac = re.search(r'\((.*)\)', macro)
+                if mac is not None:
+                    mac = mac.group(1)
+                else:
+                    mac = ""
+
+                if mac is not None and len(mac) > 0:
+                    vl = theRow[mac]
+                else:
+                    vl = ""
+
+                if len(fn) > 0:
+                    if mac is not None and len(mac) > 0:
+                        theExp = f"{fn}(vl)"
+                    else:
+                        theExp = f"{fn}()"
+                    vl = eval(theExp)
+                tmplt = tmplt.replace(macro, str(vl))
+            return tmplt
+        # end inner functions
+
+        dfRows = df.shape[0]
+        dfCols = df.shape[1]
+        dfTemps = len(templates)
+        doCache = False
+        tnCount = 0
+
+        for i in range(dfRows):
+            for pair in templates:
+                tnT = pair[0]
+                tagT = pair[1]
+
+                macros = re.findall(r'\$[a-zA-Z0-9_]*\([a-zA-Z0-9_\s]*\)', tnT)
+                tnT = doTemplates(macros, tnT, df.iloc[i])
+
+                macros = re.findall(r'\$[a-zA-Z0-9_]*\([a-zA-Z0-9_]*\)', tagT)
+                tagT = doTemplates(macros, tagT, df.iloc[i])
+                tagList = []
+                if len(tagT) > 0:
+                    tagList = [tag.strip() for tag in tagT.split(",")]
+                postStatement(tnT, tagList)
+                tnCount += 1
+
+        print(f"{tnCount} TNs written")
+
+    @staticmethod
+    def pathLength(path):
+        return len(re.findall(r'[:/]', path)) + 1
+
+    @staticmethod
+    def subPath(path, n=1):
+        p = [(m.start(), m.end()) for m in re.finditer(r'[:/]', path)]
+        return path[:p[n-1][0]]
+
